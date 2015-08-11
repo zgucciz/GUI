e85d2ab4b80b6afff1601c7f544379b99dd22ab5
zgucciz <zgamez@mail.ru>
2015-08-12 01:08:48 +0400

    with small changes

--- src/main/java/net/schastny/myspringtemplate/client.java
+++ src/main/java/net/schastny/myspringtemplate/client.java
@@ -1,5 +1,7 @@
 package net.schastny.myspringtemplate;
 
+import javax.swing.JOptionPane;
+
 import org.springframework.web.client.RestTemplate;
 import fileSystem.Folder;
 
@@ -9,8 +11,12 @@ public class client {
 	public static void main(String[] args){
 		RestTemplate restTemplate = new RestTemplate();
 		Folder f;
-		f = restTemplate.getForObject("http://localhost:8080/root", Folder.class);
+		try{
+		f =(Folder) restTemplate.getForObject("http://localhost:8080/root", Folder.class);
 		System.out.println(f.getName());
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, e, "ERROR!", 0);
+		}
 	}
 
 }
